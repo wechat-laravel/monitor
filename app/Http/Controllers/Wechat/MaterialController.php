a4de3b44d82fed062a454777f0f92af228da885b
hackqy <hackqy@qq.com>
2016-10-13 11:58:24 +0800

    material

---
+++ app/Http/Controllers/Wechat/MaterialController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Wechat;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MaterialController extends Controller
+{
+    
+
+}
--- app/Http/Controllers/Wechat/UsersController.php
+++ app/Http/Controllers/Wechat/UsersController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Wechat;
 
 use EasyWeChat\Foundation\Application;
 use Illuminate\Http\Request;
