9f74867bc902b7dbda9451cd2834e3fcf12ec3dc
hackqy <hackqy@qq.com>
2016-10-13 15:58:06 +0800

    material

--- app/Http/Controllers/Wechat/MenuController.php
+++ app/Http/Controllers/Wechat/MenuController.php
@@ -18,8 +18,12 @@ class MenuController extends Controller
     public function __construct(Application $menu){
         $this->menu = $menu->menu;
     }
+    //获取普通菜单
+    public function menu(){
+        return $this->menu->all();
+    }
 
-    //
+    //获取自定义菜单
     public function menus(){
         return $this->menu->current();
 
@@ -27,26 +31,61 @@ class MenuController extends Controller
     public function add(){
         $buttons = [
             [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name"       => "菜单",
+                "name"       => "简介",
                 "sub_button" => [
                     [
                         "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
+                        "name" => "关于author",
+                        "url"  => "http://hackqy.com/"
                     ],
                     [
                         "type" => "view",
-                        "name" => "视频",
-                        "url"  => "http://v.qq.com/"
+                        "name" => "关于博客",
+                        "url"  => "http://hackqy.com/"
                     ],
                     [
                         "type" => "click",
                         "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+            [
+                "name"       => "管理系统",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://hackqy.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://hackqy.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+            [
+                "name"       => "监控系统",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://hackqy.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://hackqy.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "pyspider速度",
                         "key" => "V1001_GOOD"
                     ],
                 ],
