66a1a02f7e420a4070bf2b028895a3affb745cb3
hackqy <hackqy@qq.com>
2016-10-15 15:00:54 +0800

    pyspider

---
+++ app/Pyspider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pyspider extends Model
+{
+    /**
+     * 关联到模型的数据表
+     * @var string
+     */
+    protected  $table = 'one_analysis';
+
+
+}
