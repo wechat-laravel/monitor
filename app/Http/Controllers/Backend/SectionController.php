2b685e5b8886013e1c37377f04bcd9e30ad5026e
hackqy <hackqy@qq.com>
2016-10-28 22:06:59 +0800

    screen

--- app/Http/Controllers/Backend/SectionController.php
+++ app/Http/Controllers/Backend/SectionController.php
@@ -41,6 +41,10 @@ class SectionController extends Controller
             ->select('sn')
             ->orderby('id','desc')
             ->first();
+        if(empty($res)){
+            $id = 0;
+            return $id;
+        }
         $id = DB::table(env('DB_SCREEN'))
             ->select('id')
             ->where('sn','=',$res->sn)
@@ -136,12 +140,24 @@ class SectionController extends Controller
         //开始计算比值的起始时间
         //时间都转换成24小时制的整数
         $incr [] = ['sn'=>$this->sn,'times'=> intval(substr(date('Y/m/d H:i:s',$this->time_hour[23]['time']),11,2)) ,'ratio'=>0 ];
+        //计算比值总和,如果等于零,表示该监控记录有误
+        $num = 0;
         for ($i = 23;$i>0;$i--){
             $time = date('Y/m/d H:i:s',$this->time_hour[$i-1]['time']);
             $time = intval(substr($time,11,2));
             //ratio 比值 都是千分数,乘1000 四舍五入 保留为整数
-            $incr[] = ['sn'=>$this->sn,'times'=>$time ,'ratio'=>intval(round((($this->time_hour[$i-1]['num'] - $this->time_hour[$i]['num'])/$sum)*1000))];
+            if($this->time_hour[$i-1]['num'] - $this->time_hour[$i]['num'] != 0){
+                $ratio=intval(round((($this->time_hour[$i-1]['num'] - $this->time_hour[$i]['num'])/$sum)*1000));
+            }else{
+                $ratio=0;
+            }
+            $incr[] = ['sn'=>$this->sn,'times'=>$time ,'ratio'=>$ratio];
+            $num = $num + $ratio;
         }
+        if($num == 0){
+            return true;
+        }
+//        var_dump($incr);exit;
         //添加到数据库中,并返回bool值
         return DB::table(env('DB_RATIO'))->insert($incr);
 
@@ -149,6 +165,10 @@ class SectionController extends Controller
 
     //分段 数组创建
     public function section($timestamp){
+        //如果数组不为空 清空
+        if(!empty($this->time_hour)){
+            $this->time_hour = [];
+        }
         for($i=0;$i<25;$i++){
             $this->time_section[] = ['time'=>$timestamp-(3600*$i),'num'=>0];
         }
@@ -156,6 +176,10 @@ class SectionController extends Controller
 
     //整点 数组创建
     public function hours($timestamp){
+        //如果数组不为空 清空
+        if(!empty($this->time_hour)){
+            $this->time_hour = [];
+        }
         date_default_timezone_set('Asia/Shanghai');
         $time =  date('Y/m/d H:i:s',$timestamp);
         $new_time = substr_replace($time,'00:00',14,5);
