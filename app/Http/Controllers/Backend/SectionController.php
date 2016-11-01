beec835fdac75799fe32af48d26172ed029eb085
hackqy <hackqy@qq.com>
2016-11-01 14:46:40 +0800

    screen

--- app/Http/Controllers/Backend/SectionController.php
+++ app/Http/Controllers/Backend/SectionController.php
@@ -12,10 +12,7 @@ class SectionController extends Controller
     //记录最后时间的前的24个小时时间  增量比值
     protected $time_section = [];
     //记录该监控所在的整点时间段内的  增量比值
-    protected $time_hour    = [];
-
-    //最终阅读量
-    protected $end_read_num ;
+    public  $time_hour    = [];
 
     //文章的唯一标识
     protected $sn ;
@@ -25,12 +22,13 @@ class SectionController extends Controller
         $id  = $this->getId();
         $now = time();
         $res = DB::table(env('DB_SCREEN'))
-            ->select('sn','post_at','begin_at')
+            ->select('wechat_id','sn','post_at','begin_at')
             ->where('end_at','<',$now)
             ->where('days','=',1)
             ->where('id','>',$id)
-            ->offset(500)
+            ->offset(0)
             ->limit(500)
+//            ->where('sn','=','77c9cd42c144e23a37ce0a1dd5bae33d')
             ->get();
 //        return $res;
         if(empty($res)){
@@ -68,15 +66,18 @@ class SectionController extends Controller
             $res = DB::table(env('DB_SCREEN_RESULT'))
                 ->select('sn', 'read_num', 'like_num', 'updated_at')
                 ->where('sn', '=', $ob->sn)
-                ->orderby('updated_at', 'desc')
+                ->orderby('updated_at', 'asc')
                 ->get();
+
             //监控的时候出现错误也会造成没有监控的情况
             if(empty($res)) continue;
-            //updated_at是按照倒叙来排的  如果记录开始监控的时间与真实开始监控的时间相差一个小时 跳过
-            if( end($res)->updated_at - $post_time >3600) continue;
+            $num = end($res)->read_num;
+            //阅读量的范围
+            if ($num < 8000 || $num > 100000) continue;
 
             foreach ($res as $re) {
-
+                //updated_at是按照正叙来排的  如果记录开始监控的时间与真实开始监控的时间相差一个小时 跳过
+                if ($re->updated_at - $post_time > 3600) break;
 
                 //判断该sn是否已经存在表中,存在跳过
                 $exists = DB::table(env('DB_RATIO'))
@@ -85,18 +86,13 @@ class SectionController extends Controller
                     ->first();
                 if(!empty($exists)) break;
 
-                if ($re->read_num >= 8000 && $re->read_num < 100000) {
-                    $this->end_read_num = $re->read_num;
-                    $this->sn = $re->sn;
-                    $this->hours($re->updated_at);
-                    $this->monitorHours($res);
-                    $result = $this->hoursIncr();
-                    if(!$result){
-                        exit($re->sn.'有误');
-                    }else{
-                        break;
-                    }
-                }else{
+                $this->sn = $re->sn;
+                $this->hours($re->updated_at);
+                $this->monitorHours($res);
+                $result = $this->hoursIncr();
+                if (!$result) {
+                    exit($re->sn . '有误');
+                } else {
                     break;
                 }
             }
@@ -128,8 +124,8 @@ class SectionController extends Controller
             foreach ($this->time_hour as $k => $v){
                 //该数组的num会依次填空,注:empty(0)也为真
                 if(empty($v['num'])){
-                    //时间是倒叙的,所以刚满足下列条件,表示这个时刻最大的值
-                    if($ob->updated_at <= $v['time']){
+                    //监控的时间戳是正叙的,刚比这个时间段大的,表示这个时刻最大的值
+                    if($ob->updated_at >= $v['time']){
                         $this->time_hour[$k]['num'] = $ob->read_num;
                     }
                 }
@@ -149,23 +145,38 @@ class SectionController extends Controller
 
     //整点的增量比值
     public function hoursIncr(){
+        //因为有可能会出现,监控的文章不够24个小时的这种情况出现.致使末尾最终阅读量为0
+        //先倒叙,然后连续的零有几个,直到遇见的不是零为止
+        $arr = array_reverse($this->time_hour);
+        $arr_num = 0;
+        foreach ($arr as $k => $v){
+            if($v['num'] == 0){
+                $arr_num += 1;
+            }else{
+                //如果遇到不为零的直接跳出
+                break;
+            }
+        }
+        for ($i=0; $i<$arr_num; $i++){
+            array_pop($this->time_hour);
+        }
+        $arr_count = count($this->time_hour)-1;
         //增量
-        $sum = $this->time_hour[0]['num'] - $this->time_hour[23]['num'];
-        //开始计算比值的起始时间
-        //时间都转换成24小时制的整数
-//        $incr [] = ['sn'=>$this->sn,'times'=> intval(substr(date('Y/m/d H:i:s',$this->time_hour[23]['time']),11,2)) ,'ratio'=>0 ];
+        $sum = $this->time_hour[$arr_count]['num'] - $this->time_hour[0]['num'];
         //计算比值总和,如果等于零,表示该监控记录有误
         $num = 0;
-        for ($i = 23;$i>0;$i--){
-            $time = date('Y/m/d H:i:s',$this->time_hour[$i-1]['time']);
+        $incr = [];
+        for ($i = 0;$i<$arr_count;$i++){
+            $time = date('Y/m/d H:i:s',$this->time_hour[$i+1]['time']);
             $time = intval(substr($time,11,2));
             //ratio 比值 向上保留万分数 除以100
-            if($this->time_hour[$i-1]['num'] - $this->time_hour[$i]['num'] != 0){
-                $ratio=intval((ceil((($this->time_hour[$i-1]['num'] - $this->time_hour[$i]['num'])/$sum)*10000)));
+            if($this->time_hour[$i+1]['num'] - $this->time_hour[$i]['num'] != 0){
+                $ratio=intval((ceil((($this->time_hour[$i+1]['num'] - $this->time_hour[$i]['num'])/$sum)*10000)));
             }else{
                 $ratio=0;
             }
             $incr[] = ['sn'=>$this->sn,'times'=>$time ,'ratio'=>$ratio];
+
             $num = $num + $ratio;
         }
         if($num == 0){
@@ -190,16 +201,16 @@ class SectionController extends Controller
 
     //整点 数组创建
     public function hours($timestamp){
-        //如果数组不为空 清空
-        if(!empty($this->time_hour)){
-            $this->time_hour = [];
-        }
+        //先清空该数组
+        $this->time_hour = [];
+
         date_default_timezone_set('Asia/Shanghai');
         $time =  date('Y/m/d H:i:s',$timestamp);
+        //如果等于23的话,+1 要取00点 并且前面日期也要+1
         $new_time = substr_replace($time,'00:00',14,5);
-        $new_timestamp =  strtotime($new_time);
+        $new_timestamp =  strtotime($new_time)+3600;
         for($i=0;$i<24;$i++){
-            $this->time_hour[] = ['time'=>$new_timestamp-(3600*$i),'num'=>0];
+            $this->time_hour[] = ['time'=>$new_timestamp+(3600*$i),'num'=>0];
         }
     }
 
