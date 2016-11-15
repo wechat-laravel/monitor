ce3bad32f7f922d33c700d5c45f8f74e2b117fa2
hackqy <hackqy@qq.com>
2016-11-15 09:17:48 +0800

    sign

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,6 +38,8 @@ class RouteServiceProvider extends ServiceProvider
     public function map(Router $router)
     {
         $router->group(['namespace' => $this->namespace], function ($router) {
+            //可以一次require多个routes_xx.php文件
+            //如果是分域名的话,可以在这里做判断  引导
             require app_path('Http/routes.php');
         });
     }
