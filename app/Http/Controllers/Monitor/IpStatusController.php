86d2f9fce9c0730e19bc2e185d7789b35c3398ea
hackqy <hackqy@qq.com>
2016-12-21 22:28:03 +0800

    sign

--- app/Http/Controllers/Monitor/IpStatusController.php
+++ app/Http/Controllers/Monitor/IpStatusController.php
@@ -19,7 +19,6 @@ class IpStatusController extends Controller
         //
         $num = \App\Models\IpStatusModel::where('status','=',0)->count();
         return $num;
-
         
         //强迫症,打卡.该项目暂搁置,工作需要转 coding.net代码推送
     }
